759f382a576c89dd620cfe623763633f835a887e
Ritesh Sinha <r@rsinha.org>
2014-04-03 22:13:53 +0100

    User streaming works

--- ferret.py
+++ ferret.py
@@ -2,6 +2,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from utils import bcolors
+from follow import Follow
 import sys, time
 from tweepy.utils import import_simplejson, urlencode_noplus
 json = import_simplejson()
@@ -9,7 +10,8 @@ from settings import consumer_key, consumer_secret, access_token, access_token_s
 
 
 class UserStreamListener(StreamListener):
-    """ A listener handles tweets are the received from the user stream.
+    """
+    A listener handles tweets are the received on the user stream.
     """
     def on_status(self,status):
         status_wrapper = TextWrapper(width=60, initial_indent='    ', subsequent_indent='    ')
@@ -22,16 +24,17 @@ class UserStreamListener(StreamListener):
             pass
 
 
-def on_error(self, status):
-    print status
+    def on_error(self, status):
+        print status
 
-def on_timeout(self):
-    sys.stderr.write(bcolors.WARNING + "Timeout, sleeping for 60 seconds...\n" + bcolors.ENDC)
-    time.sleep(60)
-    return
+    def on_timeout(self):
+        sys.stderr.write(bcolors.WARNING + "Timeout, sleeping for 60 seconds...\n" + bcolors.ENDC)
+        time.sleep(60)
+        return
 
 class PublicStreamListener(StreamListener):
-    """ A listener that handles public stream data
+    """
+    A listener that handles public stream data
     """
     def on_data(self, raw_data):
         print raw_data
@@ -50,23 +53,28 @@ class PublicStreamListener(StreamListener):
         print "Error!"
         print status
 
-#TODO: Fix user streaming!
 def main():
-    #u = UserStreamListener()
+    # Set up the authentication!
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    #userstream = Stream(auth, u)
 
+    # Create a user and public stream
+    u = UserStreamListener()
+    userstream = Stream(auth, u)
     p = PublicStreamListener()
     publicstream = Stream(auth, p)
+    # Show my followers
+    f = Follow(auth)
+    print f.my_followers()
 
     print "Streaming started..."
     try:
-        #userstream.userstream()
+        userstream.userstream()
         publicstream.filter(track=["swsec"])
+
     except:
         print bcolors.WARNING + "Stopped streaming...." + bcolors.ENDC
-      #  userstream.disconnect()
+        userstream.disconnect()
         publicstream.disconnect()
 
 if __name__ == '__main__':
