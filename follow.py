362bc6e1da9324b89456c98bf8a8f66ba93930e0
Ritesh Sinha <r@rsinha.org>
2014-04-05 23:16:42 +0100

    Removed whitelist for now

--- follow.py
+++ follow.py
@@ -1,6 +1,7 @@
 from tweepy import API, Cursor
 
 from tweepy.utils import import_simplejson, urlencode_noplus
+
 json = import_simplejson()
 
 
@@ -8,21 +9,14 @@ class Follow():
     def __init__(self, auth):
         self.auth = auth
         self.api = API(self.auth)
+        self.whitelist = []
 
     def autofollow(self):
         """
         Auto follow everyone who follows us
         """
-        # poor man's DB, move to SQLite or something enterprise-y at some point
-        self.whitelist = []
-        with open('whitelist.txt') as f:
-            for line in f:
-                a = f.read().strip()
-                self.whitelist.append(a)
-
-        for follower in Cursor(self.api.followers()).items():
-            if follower.screen_name in self.whitelist:
-                self.api.create_friendship(follower)
+        # Re-implement autofollow
+        self.api.create_friendship(follower)
                 # We need to wait before sending a DM as this may not be instantaneous
 
     def follow(self, user):
