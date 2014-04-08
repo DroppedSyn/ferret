c209816a0d778d146d47052f16af2471ed7ae2da
Ritesh Sinha <r@rsinha.org>
2014-04-08 22:21:24 +0100

    Mock implementation for dmhandler

--- dmhandlers.py
+++ dmhandlers.py
@@ -23,6 +23,25 @@ class DMHandler():
                 if item.screen_name == screen_name:
                     out[person] = item.following
         return out
+    def verify_user(self, direct_message=None, email_verified=False):
+        """
+        Allow users to claim twitter IDs
+        if they say I am rksinha, then the DM sender is mapped to that twitter ID
+        Expects a tweepy direct message object
+        """
+        if direct_message is not None:
+            sender = direct_message["sender_screen_name"]
+            text = direct_message["text"]
+            # Use a regex match, instead of a simple split
+            email = text.split()[2]
+            # If email not in DB, return False
+            # Else map email to twitter ID but mark email verified as False in DB
+            # Send verification email
+        if email_verified is True:
+            # Mark email as verified! They are who they say they are.
+
+
+
 
     def send_dm(self, screen_name=None, id=None, message=None):
         pass
