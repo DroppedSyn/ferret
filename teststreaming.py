687af9b96d2056a14598f6aeb913bdd60c51f5db
Ritesh Sinha <r@rsinha.org>
2014-05-20 23:43:09 +0100

    Testing streaming

--- teststreaming.py
+++ teststreaming.py
@@ -3,7 +3,8 @@ from tweepy import OAuthHandler, API, Cursor
 from dmhandlers import DmCommandHandler
 from tweepy.utils import import_simplejson
 from tweepy import Stream
-
+import psycopg2
+import settings 
 json = import_simplejson()
 from settings import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
@@ -13,19 +14,20 @@ class StdOutListener(StreamListener):
 This is a basic listener that just prints received tweets to stdout.
 
     """
-    def on_error(self, status):
-        print status
-
-    def on_direct_message(self, status):
-        print status.parse()
-
+    def on_status(self, status):
+        conn = psycopg2.connect(settings.PGDBNAME)
+        cur = conn.cursor()
+        print status.text
+        #cur.execute("""INSERT INTO tweets(tweet) VALUES(status=> "%s", author
+        #        =>"%s""", (status.text, status.author.screen_name,))
+    def on_error(self, error):
+        print error
+        
 
 if __name__ == '__main__':
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     a = API(auth)
-    n = DmCommandHandler(auth)
-    print n.check_if_follows(screen_name="KimKardashian")
     l = StdOutListener()
     stream = Stream(auth, l)
-    stream.filter(track=['JustinReleaseLifeIsWorthLiving'])
+    stream.filter(track=['basketball'])
