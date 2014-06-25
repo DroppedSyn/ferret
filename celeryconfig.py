0c7095eacc9cfbd81af3b2919dc2c8b82f606797
Alex Evans <aevans@cigital.com>
2014-06-25 17:49:02 +0100

    added extra files

---
+++ celeryconfig.py
@@ -0,0 +1,14 @@
+from datetime import timedelta
+BROKER_URL = 'amqp://guest:guest@127.0.0.1:5672'
+CELERY_RESULT_BACKEND = 'db+postgresql://rann@/rann'
+CELERYBEAT_SCHEDULE = {
+        'fetch-direct-messages-every-2min': {
+            'task': 'tasks.fetchdms',
+            'schedule': timedelta(minutes=2)
+            },
+#        'check-if-follows-every2min': {
+#            'task': 'tasks.check_if_follows',
+#            'schedule': timedelta(minutes=2)
+#            },
+        }
+CELERY_TIMEZONE = 'UTC'
